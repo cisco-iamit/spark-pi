98526eb85417a08ce64976498fa45c4275792b00
Alex <me@alwye.com>
2017-11-14 15:47:49 +0000

    Setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-sudo rpi-update
-sudo raspi-config update
-sudo apt-get install -y gpac git mongodb
-pip3 install -r requirements.txt --user
+sudo apt-get install -y git mongodb
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz
+tar xzvf Python-3.6.2.tgz
+cd Python-3.6.2/
+./configure
+make
+sudo make install
+cd ~/Desktop/spark-pi/ && pip3 install -r requirements.txt --user
 cd app && mkdir tmp
